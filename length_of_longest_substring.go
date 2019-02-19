02a1e69e348ec1acc24c67a1aa336c8d3164f91f
mikoto <ele_comb@qq.com>
2019-02-19 18:37:16 +0800

    length of longest substring

--- length_of_longest_substring.go
+++ length_of_longest_substring.go
@@ -1,6 +1,44 @@
 package leetcode_golang
 
+// 4 ms	2.9 MB
 func lengthOfLongestSubstring(s string) int {
-	
-	return 0
+	var maxSubstring = make([]int32, len(s))
+	var startIndex ,endIndex, maxLength = 0, 0, 0
+	for i, v := range s {
+		endIndex = i+1
+		maxSubstring[i] = v
+		for j := startIndex; j < i; j++ {
+			if maxSubstring[j] == v {
+				if endIndex - startIndex > maxLength {
+					maxLength = endIndex - startIndex - 1
+				}
+				startIndex = j+1
+				break // !!!
+			}
+		}
+	}
+	if endIndex - startIndex > maxLength {
+		maxLength = endIndex - startIndex
+	}
+	return maxLength
+}
+
+// garcez
+func lengthOfLongestSubstring2(s string) int {
+	var longest, curStart int
+	for i, c := range s {
+		for j, c2 := range(s[curStart:i]) {
+			if c == c2 {
+				if i - curStart > longest {
+					longest = i - curStart
+				}
+				curStart += j + 1
+				break
+			}
+		}
+	}
+	if len(s) - curStart > longest {
+		longest = len(s) - curStart
+	}
+	return longest
 }
--- length_of_longest_substring_test.go
+++ length_of_longest_substring_test.go
@@ -10,8 +10,16 @@ func TestLengthOfLongestSubstring(t *testing.T) {
 		{"abcabcbb", 3 }, // abc
 		{"bbbbb", 1 }, // b
 		{"pwwkew", 3 }, // wke
+		{"aab", 2}, // ab
+		{"dvdf", 3 }, //vdf
+		{"", 0 },
+		{" ", 1 },
+		{"au", 2 },
 	}{
 		if actually := lengthOfLongestSubstring(unit.m); actually != unit.expected{
+			t.Errorf("lengthOfLongestSubstring: [%v], actually: [%v]", unit, actually)
+		}
+		if actually := lengthOfLongestSubstring2(unit.m); actually != unit.expected{
 			t.Errorf("lengthOfLongestSubstring: [%v], actually: [%v]", unit, actually)
 		}
 	}
